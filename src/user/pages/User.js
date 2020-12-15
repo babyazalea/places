d2963819b45c30ea5d6c58173ed4fb958a3b940e
babyazalea <usun12@hotmail.com>
2020-12-15 22:46:30 +0900

    users components setup

--- src/user/pages/User.js
+++ src/user/pages/User.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "ty",
+      image:
+        "https://www.mpps.co.kr/kfcs_api_img/KFCS/goods/DL_2173961_20200525160713616.png",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
