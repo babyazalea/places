f34a0dc2912953cd6c0183022f58efd4881b218a
babyazalea <usun12@hotmail.com>
2020-12-20 20:09:26 +0900

    added modal for VIEW ON THE MAP

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,38 +1,41 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 import PlaceList from "../components/PlaceList";
 
 const DUMMY_PLACES = [
   {
     id: "p1",
-    title: "LH",
-    description: "this is our home",
+    title: "song's park",
+    description: "this is song's park in front of our home.",
     imageUrl:
-      "https://cdn.pixabay.com/photo/2016/02/19/10/38/facade-1209331_960_720.jpg",
-    address: "JungangDaero 862",
+      "https://www.busaninnews.co.kr/news/photo/201709/2944_7929_2154.jpg",
+    address: "부산광역시 부산진구 전포동 전포2동 중앙대로 818",
     location: {
-      lat: 111.123123123,
-      lng: -77.123123123,
+      lat: 35.1641929,
+      lng: 129.0643827,
     },
     creator: "u1",
   },
   {
     id: "p2",
-    title: "LH",
-    description: "this is our home",
+    title: "citizen park",
+    description: "this is our favorite park",
     imageUrl:
-      "https://cdn.pixabay.com/photo/2016/02/19/10/38/facade-1209331_960_720.jpg",
-    address: "JungangDaero 862",
+      "https://www.visitbusan.net/uploadImgs/files/cntnts/20191227194943122_oen",
+    address: "부산광역시 부산진구 범전동 시민공원로 73",
     location: {
-      lat: 111.123123123,
-      lng: -77.123123123,
+      lat: 35.1641962,
+      lng: 129.0627412,
     },
     creator: "u2",
   },
 ];
 
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES} />;
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
