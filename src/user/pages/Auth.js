560c183095044656c14b7839506d212c9c740718
babyazalea <usun12@hotmail.com>
2021-05-03 22:35:14 +0900

    added firbase-hosting & adjust api-url

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -91,7 +91,7 @@ const Auth = () => {
         formData.append("password", formState.inputs.password.value);
         formData.append("image", formState.inputs.image.value);
         const responseData = await sendRequest(
-          `${process.env.REACT_APP_BACKEND_URL}api/users/signup`,
+          `${process.env.REACT_APP_BACKEND_URL}/users/signup`,
           "POST",
           formData
         );
